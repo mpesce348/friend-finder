ae4e4c8d6e8137c37fe793e3675a53deb61cbfcf
Michael Pesce <Michael Pesce>
2017-06-28 16:33:36 -0400

    updated survery

---
+++ app/data/friends.js
@@ -0,0 +1,7 @@
+var friendList = [{
+	"name" : "Mike",
+	"photo" : "https://avatars2.githubusercontent.com/u/21060740?v=3&u=ce1cb345bf450818092336d5773e02ed2e1905af&s=400",
+	"scores" :[3,3,5,1,2,4,3,3,2,3]
+}]
+
+module.exports = friendList;
